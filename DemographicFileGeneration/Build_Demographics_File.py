c47f911441e049714ac31f3f923252df72f5e594
zdu <zdu@idmod.org>
2019-03-08 09:10:48 -0800

    Measles: New implementation of Build_DemographicsFile.py by using DTK utilities

--- DemographicFileGeneration/Build_Demographics_File.py
+++ DemographicFileGeneration/Build_Demographics_File.py
@@ -4,46 +4,47 @@ Build_Demographics_File: Process results from Process_Worldpop.py and a base dem
 to create a new demographics file at ward level in northern Nigeria
 """
 #
-
+from copy import deepcopy
 import numpy as np
 import pandas as pd
 import json
+from dtk.tools.demographics.DemographicsFile import DemographicsFile
+from dtk.tools.demographics.Node import Node
 
-def node_ID_from_node_coordinates(node, res=30/3600):
-    lat = node['NodeAttributes']['Latitude']
-    long = node['NodeAttributes']['Longitude']
+def node_ID_from_lat_long(lat, long, res=30/3600):
     nodeID = int((np.floor((long+180)/res)*(2**16)).astype(np.uint) + (np.floor((lat+90)/res)+1).astype(np.uint))
     return nodeID
 
 
-def fill_nodes(out_demog, node_info, res=30/3600):
+def fill_nodes(node_info, res=30/3600):
     out_nodes = []
     states2keep = ['katsina', 'kano', 'jigawa', 'kaduna', 'bauchi']
     for index, row in node_info.iterrows():
         if row['dot_name'].split(':')[1] in states2keep:
-            curr_node = {}
-            curr_node['dot_name'] = row['dot_name']
-            curr_node['NodeAttributes'] = {}
-            curr_node['NodeAttributes']['InitialPopulation'] = int(max(5000, 1000*row['population']))
-            curr_node['NodeAttributes']['Latitude'] = row['latitude']
-            curr_node['NodeAttributes']['Longitude'] = row['longitude']
-            curr_node['NodeAttributes']['Area_deg2'] = row['area']
-            curr_node['NodeAttributes']['Area_km2'] = row['area']*111*111
-            curr_node['NodeID'] = int(node_ID_from_node_coordinates(curr_node, res))
-            if (curr_node['NodeAttributes']['InitialPopulation'] / curr_node['NodeAttributes']['Area_deg2']) > 10000000:
-                curr_node['NodeAttributes']['Urban'] = 1
-                curr_node['NodeAttributes']['BirthRate'] = 0.00024
-            else:
-                curr_node['NodeAttributes']['Urban'] = 0
-                curr_node['NodeAttributes']['BirthRate'] = 0.000288
+            pop = int(max(5000, 1000*row['population']))
+            lat = row['latitude']
+            lon = row['longitude']
+            extra_attributes = {'Area_deg2': row['area'], 'Area_km2': row['area']*111*111}
+            meta = {'dot_name': row['dot_name']}
 
-            out_nodes.append(curr_node)
+            if (pop / extra_attributes['Area_deg2']) > 10000000:
+                extra_attributes['Urban'] = 1
+                extra_attributes['BirthRate'] = 0.00024
+            else:
+                extra_attributes['Urban'] = 0
+                extra_attributes['BirthRate'] = 0.000288
+
+            node = Node(lat=lat, lon=lon, pop=pop, name=int(node_ID_from_lat_long(lat, lon, res)),
+                        forced_id=int(node_ID_from_lat_long(lat, lon, res)),
+                        extra_attributes=extra_attributes, meta=meta)
+
+            out_nodes.append(node)
     out_nodes = duplicate_nodeID_check(out_nodes)
     return out_nodes
 
 
 def duplicate_nodeID_check(nodelist):
-    nodeIDs = pd.Series([n['NodeID'] for n in nodelist])
+    nodeIDs = pd.Series([n.id for n in nodelist])
     dups = nodeIDs.duplicated()
     while any(dups):
         # In lieu of something more clever, find the first non-unique, find a nearby unused ID,
@@ -60,7 +61,11 @@ def duplicate_nodeID_check(nodelist):
                     if not any(nodeIDs.isin([testId])):
                         newNodeID = testId
         nodeIDs[ind2fix] = newNodeID
-        nodelist[ind2fix]['NodeID'] = int(newNodeID)
+        # nodelist[ind2fix]['NodeID'] = int(newNodeID)
+        n = deepcopy(nodelist[ind2fix])
+        nodelist[ind2fix] = Node(lat=n.lat, lon=n.lon, pop=n.pop,
+                        forced_id=int(newNodeID),
+                        extra_attributes=n.extra_attributes)
         dups = nodeIDs.duplicated()
     return nodelist
 
@@ -69,20 +74,16 @@ if __name__ == "__main__":
 
     # Basic parameters.  Should these be inputs?
     base_demog_file = '.\\Nigeria_LGA_demographics.json'
-    out_demog_file = '.\\Nigeria_Ward_smaller_minpop5000_demographics.json'
+    out_demog_file = '.\\Nigeria_Ward_smaller_minpop5000_demographics_new.json'
     node_info_file = '.\\population_by_ward.csv'
 
-    with open(base_demog_file, 'r') as f:
-        base_demog = json.load(f)
-    out_demog = dict()
-    out_demog['Metadata'] = base_demog['Metadata']
-    out_demog['Defaults'] = base_demog['Defaults']
-    out_demog['Defaults']['NodeAttributes']['Airport'] = 1
-    out_demog['Defaults']['NodeAttributes']['Urban'] = 0
-    out_demog['Defaults']['NodeAttributes']['BirthRate'] = 0.000288
     node_info = pd.read_csv(node_info_file)
-    out_demog['Nodes'] = fill_nodes(out_demog, node_info, res=out_demog['Metadata']['Resolution']/3600)
-    out_demog['Metadata']['NodeCount'] = len(out_demog['Nodes'])
 
-    with open(out_demog_file, 'w') as fp:
-        json.dump(out_demog, fp, indent=4)
+    dg = DemographicsFile.from_file(base_demog_file)
+    nodes = fill_nodes(node_info, res=dg.content['Metadata']['Resolution']/3600)
+    dg.nodes = {node.name: node for node in nodes}
+    dg.content['Defaults']['NodeAttributes']['Airport'] = 1
+    dg.content['Defaults']['NodeAttributes']['Urban'] = 0
+    dg.content['Defaults']['NodeAttributes']['BirthRate'] = 0.000288
+
+    dg.generate_file(out_demog_file)
